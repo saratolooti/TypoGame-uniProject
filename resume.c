8757be55c3f3a4da8e9a433ce3c9ea40fbd39848
saratolooti <saratolooeeb@gmail.com>
2021-02-02 12:40:45 +0330

    resume part

---
+++ resume.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//for resume part
+void resume(char str[50],char s[50])
+{
+    printf("\n|1|pause");
+    printf("\n\n\n\n\n\n\n\n\n\n");
+    printf("\t\t\t\t\t\t%s\n", s);
+    printf("\t\t\t\t\t\t%s", str);
+}
