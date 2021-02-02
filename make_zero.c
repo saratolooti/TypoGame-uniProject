d062ab3778d9736c13d8aede55d417397ca1dd36
saratolooti <saratolooeeb@gmail.com>
2021-02-02 12:00:26 +0330

    first initialization

---
+++ make_zero.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+// first initialization
+void make_zero(void)
+{
+    strcpy((num + n)->level,"1");
+    strcpy((num + n)->score,"0");
+    strcpy((num + n)->words,"0");
+}
