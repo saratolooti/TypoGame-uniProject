63a9c5f719e07b133b0e9f7cf72f1b5f206e09c6
saratolooti <saratolooeeb@gmail.com>
2021-02-02 12:58:56 +0330

    for the starting part

---
+++ start_level.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+//for the starting part
+void start_level(void)
+{
+    system("cls");
+    system("color 03");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
+    printf("\t\t\t\t\t\tLEVEL %d", lev);
+    Sleep(1600);
+    system("cls");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
+    printf("\t\t\t\t\t\tare you ready?");
+    Sleep(1500);
+    system("cls");
+    printf("\n\n\n\n\n\n\n\n\n\n\n");
+    printf("\t\t\t\t\t\tGO");
+    Sleep(1000);
+    system("cls");
+}
