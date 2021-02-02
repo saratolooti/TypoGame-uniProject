e71b2f87ec4eaad0488e44dd1fa93c640425529c
saratolooti <saratolooeeb@gmail.com>
2021-02-02 12:09:40 +0330

    These functions turn the char into int and vice versa

---
+++ char_int.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//convert characters to integrators
+void char_to_int(void)
+{
+   num_word = atoi((num + n)->words);
+   lev = atoi((num + n)->level);
+   score_ = atoi((num + n)->score);
+}
+
+//convert integrators to characters
+void int_to_char(void)
+{
+    itoa(num_word,(num + n)->words,10);
+    itoa(lev,(num + n)->level,10);
+    itoa(score_,(num + n)->score,10);
+}
